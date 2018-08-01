bcb00c1dd4201e853f33e647b3a66a550defaf7c
DHOCNETwork <desktop.hobbie@gmail.com>
2018-08-01 00:17:37 +0000

    Update slacknetinst.sh
    perubahan beberapa opsi

--- scripts/netinstall/slackware/slacknetinst.sh
+++ scripts/netinstall/slackware/slacknetinst.sh
@@ -9,13 +9,23 @@
 #
 # Tanggal       : 31 Juli 2018
 
+# mengaktifkan teks blinkblink - text formating
 shopt -s xpg_echo
 
-PKG_MINI="a/aaa_base a/aaa_elflibs a/aaa_terminfo a/acl a/attr a/bash a/bin a/btrfs-progs a/bzip2 a/coreutils a/dbus a/dcron a/devs a/dialog a/e2fsprogs a/ed a/etc a/file a/findutils a/hostname a/hwdata a/lbzip2 a/lvm2 a/less a/gawk a/gettext a/getty-ps a/glibc-solibs a/glibc-zoneinfo a/gptfdisk a/grep a/gzip a/jfsutils a/inotify-tools a/kmod a/lrzip a/lzip a/lzlib a/mtd-utils a/pkgtools a/procps-ng a/reiserfsprogs a/shadow a/sed a/sysklogd a/sysvinit a/sysvinit-scripts a/tar a/eudev a/libgudev a/usbutils a/util-linux a/vboot-utils a/which a/xfsprogs a/xz ap/groff ap/man-db ap/man-pages ap/nano ap/slackpkg d/perl n/openssl n/ca-certificates n/dhcpcd n/gnupg n/lftp n/libmnl n/network-scripts n/nfs-utils n/ntp n/iputils n/net-tools n/iproute2 n/openssh n/rpcbind n/libtirpc n/rsync n/telnet n/traceroute n/wget n/wpa_supplicant n/wireless-tools l/lzo l/libnl3 l/libidn l/libunistring l/mpfr l/ncurses l/pcre"
+# paket miniroot
+PKG_MINI="a/aaa_base a/aaa_elflibs a/aaa_terminfo a/acl a/attr a/bash a/bin a/btrfs-progs a/bzip2 a/coreutils a/dbus a/dcron a/devs a/dialog a/e2fsprogs a/ed a/etc a/file a/findutils a/hostname a/hwdata a/lbzip2 a/lvm2 a/less a/gawk a/gettext a/getty-ps a/glibc-solibs a/glibc-zoneinfo a/gptfdisk a/grep a/gzip a/jfsutils a/inotify-tools a/kmod a/lrzip a/lzip a/lzlib a/pkgtools a/procps-ng a/reiserfsprogs a/shadow a/sed a/sysklogd a/usbutils a/util-linux a/which a/xfsprogs a/xz ap/groff ap/man-db ap/man-pages ap/nano ap/slackpkg d/perl n/openssl n/ca-certificates n/dhcpcd n/gnupg n/lftp n/libmnl n/network-scripts n/nfs-utils n/ntp n/iputils n/net-tools n/iproute2 n/openssh n/rpcbind n/libtirpc n/rsync n/telnet n/traceroute n/wget n/wpa_supplicant n/wireless-tools l/lzo l/libnl3 l/libidn l/libunistring l/mpfr l/ncurses l/pcre"
+
+# slackware pkgtools modifikasi untuk digunakan pada termux
 INSTALLPKG_DL="https://raw.githubusercontent.com/dhocnet/termux/master/scripts/netinstall/slackware"
+
+# slackware pkgtools
 INSTALL_SYS=$HOME/slackware/tmp/installpkg
 UPGRADE_SYS=$HOME/slackware/tmp/upgradepkg
+
+# download folder sementara
 WGET_P=$HOME/slackware/tmp/pkg
+
+# slackware chroot script
 SCRIPT_PEMICU="IyEvZGF0YS9kYXRhL2NvbS50ZXJtdXgvZmlsZXMvdXNyL2Jpbi9iYXNoCgp1bnNldCBMRF9QUkVM
 T0FECnByb290IFwKICAgIC0tbGluazJzeW1saW5rIFwKICAgIC1PIFwKICAgIC1yICRIT01FL3Ns
 YWNrd2FyZSBcCiAgICAtYiAvZGV2LyBcCiAgICAtYiAvc3lzLyBcCiAgICAtYiAvcHJvYy8gXAog
@@ -26,7 +36,8 @@ aW46L3Vzci9iaW46L3NiaW46L3Vzci9zYmluIFwKICAgIC9iaW4vYmFzaCAtLWxvZ2luCg=="
 
 SETUP_MULAI () {
     clear
-    echo "Anda membutuhkan beberapa program lain untuk instalasi Slackware-current ARM. Yaitu \n\n 1) wget \n 2) coreutils \n 3) proot \n 4) util-linux \n 5) grep \n 6) Dialog \n 7) lzip \n"
+    # konfirmasi instalasi paket yang dibutuhkan oleh slackware pkgtools
+    echo "Anda membutuhkan beberapa program lain untuk \nmenyelesaikan instalasi Slackware-current ARM. Yaitu:\n\n 1) wget\n 2) coreutils\n 3) proot\n 4) util-linux\n 5) grep\n 6) Dialog\n 7) lzip\n"
     read -p 'Install program [Y/n]? ' ins_y
     if [ $ins_y == "n" ]
     then
@@ -38,10 +49,12 @@ SETUP_MULAI () {
 
 SETUP_BATAL () {
     clear
-    echo "Istalasi Slackware-current ARM dibatalkan!"
+    echo "Istalasi Slackware-current ARM dibatalkan!\n"
 }
 
 SETUP_TERMUX () {
+    clear
+    echo "Menginstal program yang dibutuhkan ...\n"
     apt -y upgrade && apt -y install grep coreutils lzip proot tar wget util-linux dialog
     sleep 1
     SETUP_SELECT
@@ -49,7 +62,7 @@ SETUP_TERMUX () {
 
 SETUP_SELECT () {
     clear
-    echo "-- PILIH JENIS INSTALASI -- \n\n 1) Miniroot (default) - Perlu disk 500MB \n 2) Development - Perlu disk 4GB \n"
+    echo "PILIH JENIS INSTALASI\n\n 1) Miniroot (default) - Perlu disk 500MB\n 2) Development - Perlu disk 4GB\n"
     read -p 'Pilihan (default: 1) [1/2]: ' pilih_tipe
     if [ $pilih_tipe == "2" ]
     then
@@ -62,17 +75,29 @@ INSTALL_DEFAULT () {
     clear
     mkdir -p $HOME/slackware/tmp/pkg
     echo "Mengunduh program installer: installpkg"
-    wget -c -t 0 -P $WGET_P/../ $INSTALLPKG_DL/installpkg
+    wget -c -t 0 -P $WGET_P/../ -q --show-progress $INSTALLPKG_DL/installpkg
     chmod +x $WGET_P/../installpkg
+    echo "OK."
+    echo "Mengunduh paket dasar miniroot:"
+    sleep 1
     for PKG_TODL in $PKG_MINI ; do
-        wget -c -t 0 -T 10 -w 5 -P $WGET_P -nv ftp://mirrors.slackware.bg/$ARCH_SELECT/$PKG_TODL-*.t?z
+        wget -c -t 0 -T 10 -w 5 -P $WGET_P -q --show-progress ftp://mirrors.slackware.bg/$ARCH_SELECT/$PKG_TODL-*.t?z
     done
+    echo "OK."
+    echo "Memasang sistem dasar Slackware miniroot ..."
+    sleep 2
     $INSTALL_SYS --terse --root $HOME/slackware/ $WGET_P/*.t?z
+    echo "Memeriksa pilihan Development ..."
+    sleep 1
     if [ -e $HOME/slackware/tmp/insDEV.y ]
     then
+        echo "Ditemukan!\nMelanjutkan instalasi paket Development ..."
         rm $HOME/slackware/tmp/insDEV.y
+        sleep 1
         INSTALL_DEVEL
     else
+        echo "Tidak ditemukan!\nMenyelesaikan instalasi ..."
+        sleep 1
         INSTALL_STATER
     fi
 }
@@ -81,13 +106,19 @@ INSTALL_DEVEL () {
     clear
     PKG_DEVDIR="a ap d l t"
     echo "Mengunduh program installer: upgradepkg, removepkg"
-    wget -c -t 0 -P $WGET_P/../ $INSTALLPKG_DL/{removepkg,upgradepkg}
-    chmod +x $WGET_P/../upgradepkg
+    wget -c -t 0 -P $WGET_P/../ -q --show-progress $INSTALLPKG_DL/{removepkg,upgradepkg}
+    echo "OK.\n\nMengunduh paket Development:"
+    chmod +x $WGET_P/../{removepkg,upgradepkg}
+    sleep 1
     for PKG_DEVDL in $PKG_DEVDIR ; do
-        wget -c -t 0 -r -np -nd -nv -T 10 -w 5 -A '.t{g,x}z' -P $WGET_P https://mirrors.slackware.bg/$ARCH_SELECT/$PKG_DEVDL/
+        wget -c -t 0 -r -np -nd -q --show-progress -T 10 -w 5 -A '.t{g,x}z' -P $WGET_P https://mirrors.slackware.bg/$ARCH_SELECT/$PKG_DEVDL/
     done
+    echo "OK.\nMemasang paket Development:"
+    sleep 1
     ROOT=$HOME/slackware
     $UPGRADE_SYS --install-new $WGET_P/*.t?z
+    echo "\n\nInstalasi paket Development selesai.\nMelanjutkan finishing ..."
+    sleep 1
     INSTALL_STATER
 }
 
@@ -99,6 +130,7 @@ INSTALL_STATER () {
     echo "OK ..."
     clear
     echo "Membersihkan sisa-sisa instalasi ..."
+    sleep 1
     rm -vrf $HOME/slackware/tmp/*
     echo "OK ..."
     sleep 1
@@ -107,11 +139,11 @@ INSTALL_STATER () {
 
 CARA_PAKAI () {
     clear
-    echo "SELAMAT! Anda telah berhasil memasang Slackware Linux (current-$SELECT_ARCH) di perangkat Android.\n\nUntuk menjalankan, gunakan perintah: startslack"
+    echo "SELAMAT! Anda telah berhasil memasang Slackware Linux (current-$SELECT_ARCH) di perangkat Android.\n\nUntuk menjalankan, gunakan perintah: startslack\n\n"
 }
 
 clear
-echo "\nSlackware ARM - NetInstall\nInfo: https://github.com/dhocnet/termux/tree/master/scripts/netinstall/slackware"
+echo "\nSlackware ARM - NetInstall\nInfo: https://github.com/dhocnet/termux/"
 sleep 2
 
 SELECT_ARCH=`uname -m`
